81bedbd127bd52855c316cfe48027afa1e40b2cb
Konstantin Kisilevskiy <xlonelysoul94@gmail.com>
2021-08-11 20:30:26 +0200

    Stage-5: Even game final

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,10 +3,13 @@ import prompt
 
 
 def welcome_user():
-    """brain-games welcom-cli."""
+    """Brain-games welcom-cli.
+
+    Returns:
+        str: The return value. The name of user.
+
+    """
     print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(user_name))
     return user_name
-
-
--- brain_games/even.py
+++ brain_games/even.py
@@ -5,16 +5,32 @@ import prompt
 
 
 def is_even(number):
-    """Checking a number for evenness."""
+    """Return whether a number is even.
+
+    Args:
+        number (int): Number to check.
+
+    Returns:
+        str: The return value. yes - if number is even, no - if not.
+
+    """
     if number % 2 == 0:
         return 'yes'
-    else:
-        return 'no'
+    return 'no'
+
 
 def even_game(user_name):
-    """Start the even game."""
+    """Start the even game.
+
+    Args:
+        user_name (str): The name of user.
+
+    Returns:
+        bool: The return value. False if user give wrong answer.
+
+    """
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    attempts_count =  2
+    attempts_count = 2
     while attempts_count >= 0:
         number = randrange(100)
         print('Question: {0}'.format(number))
@@ -24,6 +40,8 @@ def even_game(user_name):
             print('Correct!')
             attempts_count -= 1
         else:
-            print("'{0}' is wrong answer ;(. Correct answer was '{1}'.\nLet's try again, {2}!'".format(answer,correct_answer,user_name))
+            error_message = """'{0}' is wrong answer ;(. Correct answer was '{1}'.
+Let's try again, {2}!'"""
+            print(error_message.format(answer, correct_answer, user_name))
             return False
     print('Congratulations, {0}!'.format(user_name))
